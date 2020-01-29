73addadd54515a623328d023195c3cc2305d01e7
Jarbi Bonilla <jarbibonilla@gmail.com>
2020-01-29 06:59:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,15 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-    erb :index 
+  get '/' do 
+    erb :index
   end
   
+  post '/checkout' do 
+    session[:item] = params[:item]
+    @session = session
+    erb :checkout
+  end
   
   configure do
     enable :sessions
-    set :session_secret
+    set :session_secret, 'memes'
   end
-
-    
+  
 end
